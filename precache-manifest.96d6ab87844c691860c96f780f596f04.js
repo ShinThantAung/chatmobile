d7000f108a7050b78c71b2becbbb5d7922d94b3d
Big 9 <big9@MacBook-Pro.local>
2020-09-12 16:22:42 +0800

    Updates

---
+++ precache-manifest.96d6ab87844c691860c96f780f596f04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8949501520485663a8831d2713bc39",
+    "url": "/chatmobile/index.html"
+  },
+  {
+    "revision": "11d2f0d260d850caaad7",
+    "url": "/chatmobile/static/css/main.468ec71e.chunk.css"
+  },
+  {
+    "revision": "ecda99a4e1450c75586d",
+    "url": "/chatmobile/static/js/2.6f4f753f.chunk.js"
+  },
+  {
+    "revision": "3524326329cd6f9cdf5c8deb51b38a1f",
+    "url": "/chatmobile/static/js/2.6f4f753f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11d2f0d260d850caaad7",
+    "url": "/chatmobile/static/js/main.8454d89a.chunk.js"
+  },
+  {
+    "revision": "2922fdacffb7882a6f2d",
+    "url": "/chatmobile/static/js/runtime-main.bf09f26c.js"
+  }
+]);
